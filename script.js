2f6b7760c18eadae89a626f6fd4cc773df7a7713
Saif <saifisami2005@gmail.com>
2024-09-10 14:40:58 -0400

    Refactored the code via async functions, whilst keeping the old code in commented out.

--- script.js
+++ script.js
@@ -3,51 +3,88 @@
 const submitButton = document.querySelector("button")
 const listToPutItIn = document.getElementById("facts");
 
-function addLi(text, parent){
+function addLi(text, parent) {
     let newLi = document.createElement("li")
-        newLi.innerText = text
-        parent.append(newLi)
+    newLi.innerText = text
+    parent.append(newLi)
 }
 
 submitButton.addEventListener("click", submitButtonEventHandler)
 
-function submitButtonEventHandler(event){
+
+//The old way
+// function submitButtonEventHandler(event) {
+//     event.preventDefault();
+//     numberChosen = document.getElementById("userInput").value;
+//     listToPutItIn.innerHTML = ""
+
+//     axios.get(`http://numbersapi.com/${numberChosen}/trivia`)
+//         .then(resp => {
+//             addLi(resp.data, listToPutItIn)
+//             return axios.get(`http://numbersapi.com/${numberChosen}/math`)
+//         }
+//         )
+//         .then(resp => {
+//             addLi(resp.data, listToPutItIn)
+//             return axios.get(`http://numbersapi.com/${numberChosen}/year`)
+//         }
+//         )
+//         .then(resp => {
+//             addLi(resp.data, listToPutItIn)
+//             return axios.get(`http://numbersapi.com/${numberChosen}/date`)
+//         }
+//         )
+//         .then(resp => {
+//             addLi(resp.data, listToPutItIn)
+//         })
+//         .catch(err => console.log(err))
+// }
+
+//The new way
+async function submitButtonEventHandler(event) {
     event.preventDefault();
     numberChosen = document.getElementById("userInput").value;
     listToPutItIn.innerHTML = ""
 
-    axios.get(`http://numbersapi.com/${numberChosen}/trivia`)
-    .then(resp => {
-        addLi(resp.data, listToPutItIn)
-        return axios.get(`http://numbersapi.com/${numberChosen}/math`)
+    let promises = [
+        axios.get(`http://numbersapi.com/${numberChosen}/trivia`),
+        axios.get(`http://numbersapi.com/${numberChosen}/math`),
+        axios.get(`http://numbersapi.com/${numberChosen}/year`),
+        axios.get(`http://numbersapi.com/${numberChosen}/date`)
+    ]
+
+    let numberFacts = await Promise.all(promises)
+
+    for (let fact of numberFacts) {
+        addLi(fact.data, listToPutItIn)
     }
-)
-    .then(resp => {
-        addLi(resp.data, listToPutItIn)
-        return axios.get(`http://numbersapi.com/${numberChosen}/year`)
-    }
-)
-    .then(resp => {
-        addLi(resp.data, listToPutItIn)
-        return axios.get(`http://numbersapi.com/${numberChosen}/date`)
-    }
-)
-    .then(resp => {
-        addLi(resp.data, listToPutItIn)
-    })
-    .catch(err => console.log(err))
 }
 
 //Gets 50 random number facts from one request.
 
-anotherListToPutIn = document.getElementById("random")
 
-let randNumbStart = Math.floor((Math.random()*500))
+//The old way
+// let anotherListToPutIn = document.getElementById("random")
 
-axios.get(`http://numbersapi.com/${randNumbStart}..${randNumbStart+50}`)
-    .then(resp => {
-        for(let x = randNumbStart; x <= randNumbStart+50;x++){
-            addLi(resp.data[x], anotherListToPutIn)
-        }
+// let randNumbStart = Math.floor((Math.random() * 500))
+
+// axios.get(`http://numbersapi.com/${randNumbStart}..${randNumbStart + 50}`)
+//     .then(resp => {
+//         for (let x = randNumbStart; x <= randNumbStart + 50; x++) {
+//             addLi(resp.data[x], anotherListToPutIn)
+//         }
+//     }
+//     ).catch(err => console.log(err))
+
+//The new way
+async function fiftyNumberFacts() {
+    let anotherListToPutIn = document.getElementById("random")
+    let randNumbStart = Math.floor((Math.random() * 500))
+    let theNumbers = await axios.get(`http://numbersapi.com/${randNumbStart}..${randNumbStart + 50}`)
+
+    for (let x = randNumbStart; x <= randNumbStart + 50; x++) {
+        addLi(theNumbers.data[x], anotherListToPutIn)
     }
-)   .catch(err => console.log(err))
+}
+
+fiftyNumberFacts();
